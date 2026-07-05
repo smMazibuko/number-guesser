93ca17bb8d7df56b52639830008b77492276957a
smMazibuko <sphiwem715@gmail.com>
2026-07-05 10:42:34 +0200

    Basic game mechanics created with only one playthrough

--- src/main/java/com/smmazibuko/Main.java
+++ src/main/java/com/smmazibuko/Main.java
@@ -1,11 +1,28 @@
 package com.smmazibuko;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     static void main() {
 
-        System.out.println("Hello World");
+        System.out.println("Welcome to smMazibuko's Number Guesser!");
+
+        Random randomGenerator = new Random();
+        int finalAnswer = randomGenerator.nextInt(0, 51);
+        Scanner userInput = new Scanner(System.in);
+        int userAnswer;
+
+        System.out.println("Please guess a number between 0 and 50:");
+        userAnswer = userInput.nextInt();
+        userInput.nextLine();
+
+        if (userAnswer == finalAnswer) System.out.println("Correct! You guessed right.");
+        if (userAnswer < finalAnswer) System.out.println("Guess higher");
+        if (userAnswer > finalAnswer) System.out.println("Guess lower");
+
+        System.out.println(finalAnswer); // for debugging purposes
 
     }
 }
