53671ec5a142bc6bc5698d3de659e1bc9a3e0904
smMazibuko <sphiwem715@gmail.com>
2026-07-07 10:05:58 +0200

    Converted switch to switch expression format

--- src/main/java/com/smmazibuko/Main.java
+++ src/main/java/com/smmazibuko/Main.java
@@ -45,17 +45,11 @@ public class Main {
             }
         }
 
-        switch (userInput) {
-            case 1:
-                limit = 20;
-                break;
-            case 2:
-                limit = 50;
-                break;
-            case 3:
-                limit = 100;
-                break;
-        }
+        limit = switch (userInput) {
+            case 3 -> 100;
+            case 2 -> 50;
+            default -> limit;
+        };
 
         return limit;
     }
